3907a04f12e5378f7c609508e597ff651f19b876
tringle <johnzuo.dev@gmail.com>
2026-05-12 07:15:16 -0400

    simple list box

--- src/windows/layerListWindow.cpp
+++ src/windows/layerListWindow.cpp
@@ -8,4 +8,10 @@ LayerListWindow::LayerListWindow(std::string name) {
 
 void LayerListWindow::update() {
 	ImGui::Text("layer list");
+	const char* items[] = { "Apple", "Banana", "Cherry", "Kiwi", "Mango" };
+	static int selected_item = 0;
+
+	if (ImGui::ListBox("My List", &selected_item, items, IM_ARRAYSIZE(items), 4)) {
+		// Code to run when selection changes
+	}
 }
