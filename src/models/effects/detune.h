e7020bcbc60b2bea461daaa2fea43fb1aacf1323
NuggetsSr <ericcai1219@gmail.com>
2026-05-25 21:13:16 -0400

    detune

--- src/models/effects/detune.h
+++ src/models/effects/detune.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "../effect.h"
 #include <imgui.h>
+#include <vector>
 
 class Detune : public IEffect {
 public:
+
+	Detune(float detune) {
+		this->detune = detune;
+	}
+
 	std::pair<float, float> apply(float amp, float freq) override {
-		return {amp, freq * pow(2.0f, detune/12.0f)}
+		return { amp, freq * pow(2.0f, detune / 12.0f) };
 	}
 
 	void render() override {
-		ImGui::SliderFloat("detune (semitones)", &Detune->detune, -24.0f, 24.0f, "%.3f");
+		ImGui::SliderFloat("detune (semitones)", &detune, -24.0f, 24.0f, "%.3f");
 	}
 	float detune; // number of semitones
 
